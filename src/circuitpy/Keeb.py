515672b3e9efa515e62356a6580157d5e365f793
James Neko <james.neko@gmail.com>
2025-03-15 21:21:31 +1100

    StrimDick main class

---
+++ src/circuitpy/Keeb.py
@@ -0,0 +1,30 @@
+# All the stuff we use to send keyboard and mouse events
+import usb_hid
+# adafruit_hid is a separate library you need to get from the circuitpy site and drop in lib/
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.mouse import Mouse
+
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
+mouse = Mouse(usb_hid.devices)
+
+
+# Call with a string to type that string,
+# Call with a single keycode to tap that keycode, e.g. Keycode.F1  (See https://docs.circuitpython.org/projects/hid/en/latest/api.html#adafruit_hid.keycode.Keycode for full list)
+# Call with a list of keycodes to tap a chorded sequence, e.g. (Keycode.GUI, Keycode.S)
+def sendkeys(keys):
+  if isinstance(keys, str):
+    keyboard_layout.write(keys)
+  elif isinstance(keys, int):
+    keyboard.press(keys)
+    keyboard.release(keys)
+  elif isinstance(keys, (list, tuple)):
+    keyboard.press(*keys)
+    keyboard.release(*keys)
+
+# e.g. Mouse.LEFT_BUTTON
+def sendmouse(button):
+  mouse.click(button)
+
---
+++ src/circuitpy/StrimDick.py
@@ -0,0 +1,54 @@
+# Holds hardware configuration of our 6 buttons and LEDs, lets you add behaviours
+import time
+import microcontroller
+from digitalio import DigitalInOut, Pull
+
+from PushButton import PushButton, PushState
+
+
+class StrimDick:
+  def __init__(self, lazy, config):
+    self.buttons = []
+    self.leds = []
+    self.config = config
+    self.lazy = lazy
+    self.add_button(microcontroller.pin.GPIO3, microcontroller.pin.GPIO28)
+    self.add_button(microcontroller.pin.GPIO4, microcontroller.pin.GPIO27)
+    self.add_button(microcontroller.pin.GPIO5, microcontroller.pin.GPIO26)
+    self.add_button(microcontroller.pin.GPIO6, microcontroller.pin.GPIO23)
+    self.add_button(microcontroller.pin.GPIO7, microcontroller.pin.GPIO20)
+    self.add_button(microcontroller.pin.GPIO8, microcontroller.pin.GPIO22)
+  
+  
+  def add_button(self, sw_pin, led_pin):
+    sw_io = DigitalInOut(sw_pin)
+    sw_io.switch_to_input(pull=Pull.UP)
+    self.buttons.append(PushButton(sw_io, False))
+    led_io = DigitalInOut(led_pin)
+    led_io.switch_to_output()
+    self.leds.append(led_io)
+  
+  
+  def set_leds(self, value):
+    for led in self.leds:
+      led.value = value
+  
+  
+  def set_only_led(self, idx):
+    self.set_leds(False)
+    self.leds[idx].value = True
+  
+
+  def loop(self):
+    for button in self.buttons:
+      press_state = button.loop()    
+
+
+  def eyecatch(self, dir=1):
+    seq = range(0, 6)
+    if dir == -1:
+      seq = range(5, -1, -1)
+    for i in seq:
+      self.set_only_led(i)
+      time.sleep(0.020)
+    self.set_leds(False)
